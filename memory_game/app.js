18ed0930e47af971a0370b4da6660d4f8723a432
Nehemias Alcantara <nehemiasalcantara@Nehemiass-Air.attlocal.net>
2019-11-05 11:01:26 -0600

    created game logic

---
+++ memory_game/app.js
@@ -0,0 +1,16 @@
+// When user enters website
+// Pop up survey.
+// When user clicks the check box
+// Highlight SUBMIT button.
+// When user clicks SUBMIT
+// IF the check box has been clicked, close the survey.
+// otherwise, prompt them to answer the survey
+
+////////////////////////////////
+// Passing score = 150 points
+// Get the player's score
+
+// If the player's score is greater than or equal to the passing score
+// Display message "Current level: Passed"
+// Otherwise
+// Display message "Current level: Failed"
