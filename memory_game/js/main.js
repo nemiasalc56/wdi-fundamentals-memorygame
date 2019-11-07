335281b929f5125d4cdcae1dab1ae620d59d9f36
Nehemias Alcantara <nehemiasalcantara@Nehemiass-Air.attlocal.net>
2019-11-07 08:08:39 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,30 @@
-console.log('Up and running');
 
 var cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardOne);
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log('You found a match!');
+	} else {
+		console.log('Sorry, try again.');
+	}
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardTwo);
+ function flipCard(cardId){
 
  if (cardsInPlay.length === 2){
- 	if(cardsInPlay[0] === cardsInPlay[1]){
+ 	checkForMatch();
+ 	/*if(cardsInPlay[0] === cardsInPlay[1]){
  		alert('You found a match!');
  	}else{
  		alert('Sorry, try again.');
- 	}
+ 	}*/
  }
+
+ console.log('User flipped ' + cards[cardId]);
+ cardsInPlay.push(cards[cardId]);
+
+ };
+
+flipCard(0);
+flipCard(2);
